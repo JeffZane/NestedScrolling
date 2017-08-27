91110b0a3be0a73ef332ed288b0e81db30afc401
YangZhenggeng <Zhenggeng.Yang@nestia.com>
2017-08-28 00:00:47 +0800

    调整fling: 使用animation来实现

--- app/src/main/java/com/zhy/stickynavlayout/demo2/StickyNavLayout2.java
+++ app/src/main/java/com/zhy/stickynavlayout/demo2/StickyNavLayout2.java
@@ -1,5 +1,6 @@
 package com.zhy.stickynavlayout.demo2;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.NestedScrollingParentHelper;
@@ -7,9 +8,8 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.VelocityTracker;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.OverScroller;
@@ -19,26 +19,27 @@ import com.zhy.stickynavlayout.R;
 
 public class StickyNavLayout2 extends LinearLayout implements NestedScrollingParent {
 
+    private static final int SWIPE_SLOP = 80;
+    private static final int OPEN_MENU_DURING = 300;
+
+    private static final int STATE_OPENED = 1;
+    private static final int STATE_CLOSED = 2;
+    private static final int STATE_SCROLLED = 3; //todo: remove
+
+    private int mState;
+
     private NestedScrollingParentHelper parentHelper = new NestedScrollingParentHelper(this);
     private View mTop;
     private View mNav;
     private ViewPager mViewPager;
     private int mTopViewHeight;
     private OverScroller mScroller;
-    private VelocityTracker mVelocityTracker;
-    private int mTouchSlop;
-    private int mMaximumVelocity, mMinimumVelocity;
-    private float mLastY;
-    private boolean mDragging;
 
     public StickyNavLayout2(Context context, AttributeSet attrs) {
         super(context, attrs);
         setOrientation(LinearLayout.VERTICAL);
 
         mScroller = new OverScroller(context);
-        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
-        mMaximumVelocity = ViewConfiguration.get(context).getScaledMaximumFlingVelocity();
-        mMinimumVelocity = ViewConfiguration.get(context).getScaledMinimumFlingVelocity();
     }
 
 
@@ -48,7 +49,9 @@ public class StickyNavLayout2 extends LinearLayout implements NestedScrollingPar
 
         mTop = findViewById(R.id.id_stickynavlayout_topview);
         mNav = findViewById(R.id.id_stickynavlayout_indicator);
-        mViewPager = (ViewPager) findViewById(R.id.id_stickynavlayout_viewpager);
+        mViewPager = findViewById(R.id.id_stickynavlayout_viewpager);
+
+        mState = STATE_OPENED;
     }
 
     @Override
@@ -66,12 +69,17 @@ public class StickyNavLayout2 extends LinearLayout implements NestedScrollingPar
 
     @Override
     public void scrollTo(int x, int y) {
+        Log.e("scrollTo", "y: " + y);
+
         //限制滚动范围
-        if (y < 0) {
+        if (y <= 0) {
             y = 0;
-        }
-        if (y > mTopViewHeight) {
+            changeState(STATE_OPENED);
+        } else if (y >= mTopViewHeight) {
             y = mTopViewHeight;
+            changeState(STATE_CLOSED);
+        } else {
+            changeState(STATE_SCROLLED);
         }
 
         super.scrollTo(x, y);
@@ -85,12 +93,74 @@ public class StickyNavLayout2 extends LinearLayout implements NestedScrollingPar
         }
     }
 
-    public void fling(int velocityY) {
-        mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, mTopViewHeight);
-        invalidate();
+    public void showMenu() {
+        if (mState == STATE_CLOSED) { //or != OPEN ?
+            animScroll(getScrollY(), 0);
+        }
+    }
+
+    public void closeMenu() {
+        if (mState == STATE_OPENED) {
+            animScroll(getScrollY(), mTopViewHeight);
+        }
+    }
+
+    private void handleActionUp() {
+        if (getScrollY() >= SWIPE_SLOP) { //open
+            Log.e("handleActionUp", "---------- OPEN ----------");
+            animScroll(getScrollY(), mTopViewHeight);
+        } else {
+            Log.e("handleActionUp", "---------- CLOSE ----------");
+            animScroll(getScrollY(), 0);
+        }
+    }
+
+    private void animScroll(int start, int end) {
+        ValueAnimator anim = ValueAnimator.ofInt(start, end);
+        int during = (int) (OPEN_MENU_DURING * (Math.abs(start - end) / (float) mTopViewHeight));
+        Log.e("animScroll", "start: " + start + ", end: " + end + ", during: " + during);
+        anim.setDuration(during);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int currentValue = (int) animation.getAnimatedValue();
+                scrollTo(0, currentValue);
+            }
+        });
+        anim.start();
+    }
+
+    private void changeState(int newState) {
+        if (mState != newState) {
+            mState = newState;
+            if (mState == STATE_OPENED) {
+                Log.e("changeState", "------------- STATE_OPENED -------------");
+//                if (mPagerStateListener != null) {
+//                    mPagerStateListener.onPagerOpened();
+//                }
+            } else if (mState == STATE_CLOSED) {
+                Log.e("changeState", "------------- STATE_CLOSED -------------");
+
+//                if (mPagerStateListener != null) {
+//                    mPagerStateListener.onPagerClosed();
+//                }
+            } else {
+                Log.e("changeState", "------------- STATE_SCOLLED -------------");
+            }
+        }
     }
 
 //实现NestedScrollParent接口-------------------------------------------------------------------------
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            Log.e("onInterceptTouchEvent", "ACTION_UP ScrollY: " + getScrollY());
+            handleActionUp();
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
 
     @Override
     public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
@@ -105,6 +175,7 @@ public class StickyNavLayout2 extends LinearLayout implements NestedScrollingPar
 
     @Override
     public void onStopNestedScroll(View target) {
+        Log.e("onStopNestedScroll", "called");
         parentHelper.onStopNestedScroll(target);
     }
 
@@ -138,12 +209,7 @@ public class StickyNavLayout2 extends LinearLayout implements NestedScrollingPar
     @Override
     public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
         Log.e("onNestedPreFling", "called, scrollY: " + getScrollY());
-        if (getScrollY() > 0 && getScrollY() < mTopViewHeight) {
-            fling((int) velocityY);
-            return true;
-        } else {
-            return false;
-        }
+        return false;
     }
 
     @Override
